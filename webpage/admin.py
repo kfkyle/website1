a2ae00cff749ce5406e0fb9a7be60d8cd98c0e75
unknown <kyle.alexander.forbes@gmail.com>
2018-10-19 00:47:12 -0700

    6

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Home
+from .models import Service
 
 # Register your models here.
 
 admin.site.register(Home)
+admin.site.register(Service)
--- webpage/models.py
+++ webpage/models.py
@@ -4,9 +4,7 @@ from django.db import models
 class Home(models.Model):
     title_page = 'Title Page'
     services_header = 'Services Header'
-    service_1 = 'Service 1'
-    service_2 = 'Service 2'
-    service_3 = 'Service 3'
+    services = 'Services'
     why_us = 'Why Us'
     careers = 'Careers'
     about_us = 'About Us'
@@ -14,19 +12,48 @@ class Home(models.Model):
     home_choices = (
         (title_page, 'Title Page'),
         (services_header, 'Services Header'),
-        (service_1, 'Service 1'),
-        (service_2, 'Service 2'),
-        (service_3, 'Service 3'),
+        (services, 'Services'),
         (why_us, 'Why Us'),
         (careers, 'Careers'),
         (about_us, 'About Us'),
     )
-    image = models.ImageField(upload_to='images/', blank=True)
+
     name = models.CharField(max_length=15,
-    choices = home_choices,
-    default = title_page)
+    choices = home_choices)
+
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
-        return self.name
+        return self.title
+
+
+class Service(models.Model):
+
+    services_choices = (
+    ('Services', 'Services'),
+    ('Services Home', 'Services Home'),
+    )
+
+    icons_choices = (
+    ('fas fa-laptop-code', 'Laptop'),
+    ('fas fa-user', 'User'),
+    ('fas fa-network-wired', 'Network'),
+    ('fas fa-globe-americas', 'Globe'),
+    ('fas fa-layer-group', 'Layer'),
+    ('fas fa-chart-pie', 'Pie Chart'),
+    ('fas fa-chart-line', 'Line Chart'),
+    )
+
+    name = models.CharField(max_length=20,
+    choices = services_choices)
+    icon = models.CharField(max_length=20,
+    choices = icons_choices)
+
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return f"{self.title} - Home"
